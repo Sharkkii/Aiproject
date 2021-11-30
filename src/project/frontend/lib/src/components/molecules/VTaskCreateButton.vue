622a29b25a71235fa1084ef92b0b3aec3c234d83
Sharkkii <jaws.sharkkii@gmail.com>
2021-11-30 17:34:45 +0900

    add atoms & molecules
    - add atoms (empty directory)
    - add molecules (<v-task-create-button>)

---
+++ src/project/frontend/lib/src/components/molecules/VTaskCreateButton.vue
@@ -0,0 +1,53 @@
+<template>
+<div id="v-task-create-button">
+  <form>
+    <div class="item">
+      <label>
+        <p>Task Name</p>
+        <div><input type="text" name="task-name"></div>
+      </label>
+    </div>
+    <div class="item">
+      <label>
+        <p>Duration</p>
+        <div><input type="number" name="task-duration"></div>
+      </label>
+    </div>
+    <div class="item">
+      <label>
+        <p>Deadline</p>
+        <div><input type="number" name="task-deadline"></div>
+      </label>
+    </div>
+    <div class="item">
+      <button>
+        <p>Create</p>
+      </button>
+    </div>
+  </form>
+</div>
+</template>
+
+<style scoped lang="scss">
+.item {
+  margin-top: 10px;
+
+  input {
+    font-size: 20px;
+    height: 50px;
+    width: 100%;
+  }
+
+  button {
+    font-size: 20px;
+    height: 50px;
+    width: 100%;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "VTaskCreateButton"
+}
+</script>
