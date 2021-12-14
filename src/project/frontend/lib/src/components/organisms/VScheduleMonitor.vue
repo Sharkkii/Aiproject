e3503a04866e66788c6a01543fb0aefac9356051
Sharkkii <jaws.sharkkii@gmail.com>
2021-12-14 20:56:20 +0900

    add frames of SchedulePanel & ScheduleMonitor

---
+++ src/project/frontend/lib/src/components/organisms/VScheduleMonitor.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="v-schedule-monitor">
+  <div class="sub-header">
+    <p>Schedule Monitor</p>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+#v-schedule-monitor {
+  box-shadow: 0 0 5px $black;
+  box-sizing: border-box;
+  min-height: 400px;
+  padding: 20px;
+  width: 800px;
+}
+
+.sub-header {
+  color: $main-color;
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
+
+<script>
+export default {
+  name: "VScheduleMonitor"
+}
+</script>
---
+++ src/project/frontend/lib/src/components/organisms/VSchedulePanel.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="v-schedule-panel">
+  <div class="sub-header">
+    <p>Schedule Panel</p>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+#v-schedule-panel {
+  box-shadow: 0 0 5px $black;
+  box-sizing: border-box;
+  padding: 20px;
+  width: 400px;
+}
+
+.sub-header {
+  color: $main-color;
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
+
+<script>
+export default {
+  name: "VSchedulePanel"
+}
+</script>
