37eaf9498459874c861fabab08d54211c4614577
Sharkkii <jaws.sharkkii@gmail.com>
2021-12-17 10:35:23 +0900

    add `VReferenceTaskCreateButton`
    * this component should have been added before

---
+++ src/project/frontend/lib/src/components/molecules/VReferenceTaskCreateButton.vue
@@ -0,0 +1,72 @@
+<template>
+<div id="v-reference-task-create-button">
+  <form method="POST" v-on:submit.prevent="submit">
+    <div class="item">
+      <label>
+        <p>Task Name</p>
+        <div><input type="text" v-model="name"></div>
+      </label>
+    </div>
+    <div class="item">
+      <label>
+        <p>Required Effort</p>
+        <div><input type="number" v-model="requiredEffort"></div>
+      </label>
+    </div>
+    <div class="item">
+      <label>
+        <p>Remaining Time</p>
+        <div><input type="date" v-model="remainingTime"></div>
+      </label>
+    </div>
+    <div class="item">
+      <button type="submit">
+        <p>Create</p>
+      </button>
+    </div>
+  </form>
+</div>
+</template>
+
+<style scoped lang="scss">
+.item {
+  margin-top: 10px;
+
+  input {
+    font-size: 20px;
+    height: 50px;
+    width: 100%;
+  }
+
+  button {
+    font-size: 20px;
+    height: 50px;
+    width: 100%;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "VReferenceTaskCreateButton",
+  data: function() {
+    return {
+      name: null,
+      requiredEffort: null,
+      remainingTime: null
+    }
+  },
+  methods: {
+    createReferenceTask: function(data) {
+      this.$emit("create-reference-task", data)
+    },
+    submit: function() {
+      this.createReferenceTask({
+        name: this.name,
+        remaining_time: this.remainingTime,
+        required_effort: this.requiredEffort
+      })
+    }
+  }
+}
+</script>
