c71c62ae0e4c1ca9339a740b9cee798f18627145
Sharkkii <jaws.sharkkii@gmail.com>
2022-01-05 18:47:36 +0900

    add button molecules to `VRlPanel`
    - create window component (with multiple tabs)
    this will be segregated and defined in a SFC
    
    - add `VModelxxxxButton`s

--- src/project/frontend/lib/src/components/organisms/VRlPanel.vue
+++ src/project/frontend/lib/src/components/organisms/VRlPanel.vue
@@ -3,8 +3,34 @@
   <div class="sub-header">
     <p>RL Panel</p>
   </div>
-  <v-model-load-button v-on:load-model="loadModel">
-  </v-model-load-button>
+  <div class="window">
+    <div class="indices">
+      <div class="index">
+        <button name="train" v-on:click="toggle('train')" v-bind:checked="checked.train">
+          Train Model
+        </button>
+      </div>
+      <div class="index">
+        <button name="test" v-on:click="toggle('test')" v-bind:checked="checked.test">
+          Use Pretrained Model
+        </button>
+      </div>
+    </div>
+    <div class="tab train" v-if="checked.train">
+      <v-model-init-button>
+      </v-model-init-button>
+      <v-model-setup-button>
+      </v-model-setup-button>
+      <v-model-train-button>
+      </v-model-train-button>
+      <v-model-save-button>
+      </v-model-save-button>
+    </div>
+    <div class="tab test" v-if="checked.test">
+      <v-model-load-button v-on:load-model="loadModel">
+      </v-model-load-button>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -22,16 +48,65 @@
   font-size: 30px;
   font-weight: bold;
 }
+
+.indices {
+  display: flex;
+}
+
+.index {
+
+  width: 50%;
+
+  button {
+
+    box-sizing: border-box;
+    font-size: 20px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    width: 100%;
+    &[checked] {
+      border-width: medium;
+      font-weight: bold;
+    }
+
+  }
+
+}
 </style>
 
 <script>
+import VModelInitButton from '../molecules/VModelInitButton.vue'
+import VModelSetupButton from '../molecules/VModelSetupButton.vue'
+import VModelTrainButton from '../molecules/VModelTrainButton.vue'
 import VModelLoadButton from '../molecules/VModelLoadButton.vue'
+import VModelSaveButton from '../molecules/VModelSaveButton.vue'
 export default {
   name: "VRlPanel",
   components: {
-    VModelLoadButton
+    VModelInitButton, VModelSetupButton, VModelTrainButton, VModelLoadButton, VModelSaveButton
+  },
+  data: function() {
+    return {
+      checked: {
+        "train": true,
+        "test": false
+      }
+    }
   },
   methods: {
+    toggle: function(name) {
+      let self = this;
+      if (this.checked.hasOwnProperty(name)) {
+        Object.keys(self.checked).forEach(function(key) {
+          if (key === name) {
+            self.checked[key] = true;
+          } else {
+            self.checked[key] = false;
+          }
+        })
+      }
+    },
     loadModel: function(data) {
       this.$emit("load-model", data);
     }
